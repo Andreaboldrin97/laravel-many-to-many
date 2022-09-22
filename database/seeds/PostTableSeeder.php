688b957e4721432bbdb79dbfc0503ea139c41001
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-22 17:32:39 +0200

    create post default

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -18,6 +18,17 @@ class PostTableSeeder extends Seeder
     {
         $user = User::all();
         $category = Category::all();
+        $myUserId = User::WHERE('email', 'root@gmail.com')->get();
+
+        $newMyPost = new Post();
+        $newMyPost->category_id = $faker->randomElement($category)->id;
+        $newMyPost->user_id = $myUserId->id;
+        $newMyPost->title = 'My Tutor';
+        $newMyPost->description = 'orem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nunc augue, vehicula non tincidunt nec, pretium at tellus. Nam posuere nulla erat, ut tincidunt mauris scelerisque nec. Vestibulum porttitor dui vitae quam volutpat porta. Curabitur non elit diam. Donec non orci quam. Fusce in volutpat lectus. Aenean eget ante lacus.';
+        $newMyPost->image_url = 'https://ca.slack-edge.com/T91QPE3BP-U02HYRKL8E5-ca77aa4493a4-72';
+        $newMyPost->sale_date  = '2022-07-22 08:00:00';
+        $newMyPost->save();
+
 
         for ($i = 1; $i <= 20; $i++) {
             $newPost = new Post();
